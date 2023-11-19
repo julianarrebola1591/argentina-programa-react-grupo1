e17e662a92941f8603e785f37d6725cd2ebceeb8
julianarrebola1591 <julianarrebolapc99@gmail.com>
2023-11-19 00:29:29 -0300

    Pruebas de componentes

--- proyecto-final/src/App.jsx
+++ proyecto-final/src/App.jsx
@@ -2,8 +2,26 @@ import './App.css'
 
 function App() {
 
+  const TaskItem = () => {
+    return (
+      <h2>Tarea</h2>
+    )
+  }
+
+  const TaskList = () => {
+    return (
+      <div>
+        <h1>Lista de Tareas</h1>
+        <input type="text" />
+        <button></button>
+        <TaskItem />
+      </div>
+    )
+  }
+
   return (
     <>
+      <TaskList />
     </>
   )
 }
